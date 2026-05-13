3ddad32e78245a44ba9f2116bef6dab402e7dc27
shrouqNahar <shineyousf@gmail.com>
2026-05-13 07:48:14 +0300

    Create dataset.py

---
+++ data/dataset.py
@@ -0,0 +1,15 @@
+#data1
+import kagglehub
+
+
+path = kagglehub.dataset_download("manjilkarki/deepfake-and-real-images")
+
+print("Path to dataset files:", path)
+
+#data2
+
+import kagglehub
+
+path = kagglehub.dataset_download("kshitizbhargava/deepfake-face-images")
+
+print("Path to dataset files:", path)
