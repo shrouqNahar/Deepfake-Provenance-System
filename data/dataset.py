33cfbb30383bdbb931488b306c3929d4442cb5f1
shrouqNahar <shineyousf@gmail.com>
2026-05-22 07:46:09 +0300

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,15 +1,31 @@
-#data1
+# DATASET 1
+# =================Like Dataset 1============================
+https://www.kaggle.com/datasets/manjilkarki/deepfake-and-real-images
+
+# =============DOWNLOAD CODE DATASET 1========================
+
 import kagglehub
-
-
 path = kagglehub.dataset_download("manjilkarki/deepfake-and-real-images")
-
 print("Path to dataset files:", path)
+#===========================================================================
 
-#data2
+# DATASET 2
+# ================Like Dataset 2===========================
+https://www.kaggle.com/datasets/kshitizbhargava/deepfake-face-images
+
+# =============DOWNLOAD CODE DATASET 2======================
 
 import kagglehub
-
 path = kagglehub.dataset_download("kshitizbhargava/deepfake-face-images")
+print("Path to dataset files:", path)
+#=================================================================================
 
+# DATASET 3
+# =================Like Dataset 3==============================
+https://www.kaggle.com/datasets/pranabr0y/celebdf-v2image-dataset
+
+# ===============DOWNLOAD CODE DATASET 3=================
+
+import kagglehub
+path = kagglehub.dataset_download("pranabr0y/celebdf-v2image-dataset")
 print("Path to dataset files:", path)
